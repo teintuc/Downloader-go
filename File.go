0b266cacd52fc8abe4c885f6803b30736a686798
Charles Teinturier <teintu.c@gmail.com>
2016-11-11 22:17:18 +0100

    Initial commit

---
+++ File.go
@@ -0,0 +1,59 @@
+package downloader
+
+import (
+	"fmt"
+	"io"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type PassThru struct {
+	io.Reader
+	total         int64
+	contentLength int64
+	printProgress bool
+}
+
+func progress(current, total, bar_size int) {
+	amount := int(float32(current) / (float32(total) / float32(bar_size)))
+	remain := bar_size - amount
+	progress := (current * 100) / total
+
+	bar := " [" + strings.Repeat("=", amount) + strings.Repeat(" ", remain) + "] " + strconv.Itoa(current) + " " + strconv.Itoa(progress) + "%"
+	fmt.Println(bar)
+}
+
+func (pt *PassThru) Read(p []byte) (int, error) {
+	n, err := pt.Reader.Read(p)
+	if err == nil && pt.printProgress == true && pt.contentLength > 0 {
+		pt.total += int64(n)
+		progress(int(pt.total), int(pt.contentLength), 25)
+	}
+	return n, err
+}
+
+func DownloadFile(url string, filename string) (err error) {
+	/* Create a GET resousrce to download the content */
+	response, err := NewGetClient(url)
+	if err != nil {
+		return err
+	}
+	defer response.Body.Close()
+
+	/* Create the targeted file */
+	fd, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer fd.Close()
+
+	fmt.Println("Downloading to: " + filename)
+	/* Copy the content into the targeted file */
+	src := &PassThru{Reader: response.Body, contentLength: response.ContentLength, printProgress: true}
+	_, err = io.Copy(fd, src)
+	if err == nil {
+		fmt.Println("Done....")
+	}
+	return err
+}
